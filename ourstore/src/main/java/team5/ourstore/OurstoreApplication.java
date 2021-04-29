374c8a381b5e4754f66a0732fcabc02731e1aae9
Arturo Granados <agranados0121@gmail.com>
2021-04-29 13:51:55 -0500

    added gradle file and needed structures

---
+++ ourstore/src/main/java/team5/ourstore/OurstoreApplication.java
@@ -0,0 +1,13 @@
+package team5.ourstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurstoreApplication.class, args);
+	}
+
+}
---
+++ ourstore/src/test/java/team5/ourstore/OurstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package team5.ourstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
