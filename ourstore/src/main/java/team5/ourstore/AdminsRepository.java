142988a9c9017660351126838a27398cb10f5e4a
Arturo Granados <agranados0121@gmail.com>
2021-05-02 21:48:24 -0500

    Added main and admin classes

---
+++ ourstore/src/main/java/team5/ourstore/AdminsRepository.java
@@ -0,0 +1,7 @@
+package team5.ourstore;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdminsRepository extends CrudRepository<Admins, Integer> {
+    
+}
---
+++ ourstore/src/main/java/team5/ourstore/Main.java
@@ -0,0 +1,10 @@
+package team5.ourstore;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+    }
+}
