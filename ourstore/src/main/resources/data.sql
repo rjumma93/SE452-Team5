d41aebbb29e4d854e1d143d52c43605c2affb896
Arturo Granados <agranados0121@gmail.com>
2021-05-25 19:35:58 -0500

    added 2 users

--- ourstore/src/main/resources/data.sql
+++ ourstore/src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO Customer(id, first_name, last_name, email, passwords) VALUES
-(1, 'John', 'Smith' , 'jsmith@gmail.com', '1234');
+(1, 'John', 'Smith' , 'jsmith@gmail.com', '1234'),
+(2, 'Jane', 'Doe', 'jdoe123@gmail.com', 'password');
